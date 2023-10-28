51304a516460713023cd687055230ba4c9f64d7c
nikhil kumar <nikhilkrsolanki@gmai.com>
2023-10-28 07:02:47 +0530

    added additional padding in signup form

--- src/pages/auth/AuthLayout.tsx
+++ src/pages/auth/AuthLayout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout() {
     <div className="w-full h-screen px-2 relative bg-cover bg-center z-20" style={{backgroundImage:`url(${authbg})`}}>
           <div className='absolute bottom-0 left-0 right-0 h-[40%] bg-gradient-to-t from-[#673BE7] to-transparent -z-10'/>
           <div className='absolute top-0 left-0 right-0 h-full bg-gradient-to-b from-black to-transparent -z-10'/>
-          <div className='mx-auto max-w-md py-40'>
+          <div className='mx-auto max-w-md py-28 sm:py-40'>
             <Outlet/>
           </div>
     </div>
